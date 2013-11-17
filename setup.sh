a20053a816fdd8345bdaa677f133e4644e60927c
Antoine Grondin <antoinegrondin@gmail.com>
2013-11-17 18:00:19 -0500

    Fix Go setup.

--- setup.sh
+++ setup.sh
@@ -3,13 +3,20 @@ ln -s $(pwd)/tmux.conf $HOME/.tmux.conf
 ln -s $(pwd)/vimrc $HOME/.vimrc
 ln -s $(pwd)/vim $HOME/.vim
 
-# Copy/update Go files, if Go is installed
-if [-n "$GOROOT"]; then
-	cp -r $GOROOT/misc/vim $(pwd)/vim
-else
-	echo "Go is not installed... you should install it.
-fi
-
 # Append zshrc shared settings to current zshrc
 echo "source $HOME/dotfiles/zshrc" >> $HOME/.zshrc
 
+# Copy/update Go files, if Go is installed
+if [ -n "$GOROOT" ]; then
+	cp -r $GOROOT/misc/vim $(pwd)
+
+	# Set gocode
+	go get -u github.com/nsf/gocode	
+#	cp -r $GOPATH/src/github.com/nsf/gocode/vim $(pwd)
+
+	# Set golint
+	go get -u github.com/golang/lint/golint
+#	cp -r $GOPATH/src/github.com/golang/lint/misc/vim $(pwd)
+else
+	echo "Go is not installed... you should install it."
+fi
--- vimrc
+++ vimrc
@@ -1,1 +1,14 @@
-colorscheme eink
+set number
+set nocompatible
+syntax on
+
+filetype plugin on
+filetype on
+filetype indent on
+
+set rtp+=$GOROOT/misc/vim
+set rtp+=$GOPATH/src/github.com/golang/lint/misc/vim
+set rtp+=$GOPATH/src/github.com/gocode/vim
+
+autocmd BufWritePost,FileWritePost *.go execute 'Lint' | cwindow
+autocmd BufWritePre *.go Fmt
