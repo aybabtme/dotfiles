845e80c17ea20df1aafb9fdf14b6d821922e29fe
Antoine Grondin <antoinegrondin@gmail.com>
2014-07-23 15:55:28 -0400

    dd gopath/src to cdpath

--- fish/config.fish
+++ fish/config.fish
@@ -9,8 +9,9 @@ set -xg PATH "/Applications/Postgres.app/Contents/Versions/9.3/bin" $PATH
 set -xg PATH "$HOME/go/bin" $PATH
 
 # Homebrew install folder should come first
-set -xg PATH /usr/local/bin:/usr/local/sbin $PATH
-
+if test -d "/usr/local/sbin"
+  set -xg PATH $PATH "/usr/local/sbin"
+end
 
 set -xg PKG_CONFIG_PATH $PKG_CONFIG_PATH /usr/local/Cellar/sqlite/3.8.4.3/lib/pkgconfig
 set -xg PKG_CONFIG_PATH /usr/local/Cellar/qt5/5.1.0/lib/pkgconfig/ $PKG_CONFIG_PATH
@@ -18,23 +19,13 @@ set -xg PKG_CONFIG_PATH /usr/local/Cellar/qt5/5.1.0/lib/pkgconfig/ $PKG_CONFIG_P
 
 set -xg PATH "$HOME/bin" "/usr/local/bin" $PATH
 
-if test -d "$HOME/gocode/bin"
-  set -xg PATH "$HOME/gocode/bin" $PATH
-end
+set -xg GOPATH "$HOME/gocode" $GOPATH
+set -xg PATH "$GOPATH/bin" $PATH
+set -xg CDPATH $CDPATH "$GOPATH/src"
 
-if test -d "/usr/local/sbin"
-  set -xg PATH $PATH "/usr/local/sbin"
-end
-
-if test -d "/usr/local/heroku/bin"
-  set -xg PATH $PATH "/usr/local/heroku/bin"
-end
 
 set -xg EDITOR "vim"
 set -xg BUNDLE_EDITOR "mvim"
-set -xg NODE_PATH "/usr/local/lib/node" $NODE_PATH
-set -xg GOPATH "$HOME/gocode" $GOPATH
-set -xg IM_ALREADY_PRO_THANKS "conceited bastard"
 set -xg PAGER "less"
 set -xg LINEMAN_AUTO_START false
 alias vi vim
