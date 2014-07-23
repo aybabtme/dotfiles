3ced2339fb4290ec938e0b3345250c9ead298231
Antoine Grondin <antoinegrondin@gmail.com>
2014-07-23 14:00:18 -0400

    Set gitconfig, remove shitty func aliases.

--- fish/functions/alias.fish
+++ fish/functions/alias.fish
@@ -1,24 +1,3 @@
-function gb
-    git branch
-end
-
-function gco
-    git checkout
-end
-
-function gl
-    git log --graph --decorate --pretty=oneline --abbrev-commit --color='Always'
-end
-
-function gla
-    git log --graph --decorate --pretty=oneline --abbrev-commit --all
-end
-
-function gs
-    git status
-end
-
-
 function ll
     ls -Glh
 end
@@ -30,7 +9,6 @@ end
 function ls
     la
 end
-
 
 function tailf
     tail -f
